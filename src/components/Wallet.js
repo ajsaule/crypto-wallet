f7e39b944809f261ff106ba42a671fc05ff980f7
ajsaule <andrejsaule8@gmail.com>
2022-09-23 16:54:39 +1000

    wallet randomised words generated

--- src/components/Wallet.js
+++ src/components/Wallet.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Wallet.css';
 
+let defaultWords = [
+  'Lets',
+  'Gooooo',
+  'Top',
+  'Kek',
+  'Navid',
+  'Is',
+  'The',
+  'Best',
+  'Bobz',
+  'Vagene',
+];
+
 const Wallet = ({key}) => {
+  const [words, setWords] = useState(defaultWords);
+
   return (
     <div key={key} className="wallet-wrapper">
-      <div>Hello</div>
-      <div>World</div>
+      <div>{words[Math.floor(Math.random() * 10)]}</div>
+      <div>{words[Math.floor(Math.random() * 10)]}</div>
     </div>
   );
 };
